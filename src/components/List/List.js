4a4953c6b8c4b48687ee13d2727dccb281755c8b
Grzegorz Jodłowski <grzegorz.jodlowski@gmail.com>
2020-02-05 17:49:42 +0100

    Add Button and Creator components

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -8,6 +8,10 @@ import ReactHtmlParser from 'react-html-parser';
 
 
 class List extends React.Component {
+  state = {
+    columns: this.props.columns || [],
+  }
+
   static propTypes = {
     title: PropTypes.node.isRequired,
     description: PropTypes.node,
@@ -27,9 +31,9 @@ class List extends React.Component {
           {ReactHtmlParser(this.props.description)}
         </div>
         <div className={styles.columns}>
-          <Column title='To do' />
-          <Column title='Doing' />
-          <Column title='Done' />
+          {this.state.columns.map(({ key, ...columnProps }) => (
+            <Column key={key} {...columnProps} />
+          ))}
         </div>
 
       </section>
