0e9190f1517c0d9ce26a48adbc71bb41fb8de77f
Grzegorz Jodłowski <grzegorz.jodlowski@gmail.com>
2020-02-06 11:31:32 +0100

    Try to add creator to App.js file

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,15 +1,29 @@
 import React from 'react';
 import styles from './App.scss';
 import List from '../List/List.js'
-import { pageContents, listData } from '../../data/dataStore.js'
+import { pageContents, listData, settings } from '../../data/dataStore.js'
+import Creator from '../Creator/Creator.js'
+
 
 class App extends React.Component {
+
+  addList(title) {
+    this.setState(state => (
+      {
+
+      }
+    ));
+  }
+
   render() {
     return (
       <main className={styles.component}>
         <h1 className={styles.title}>{pageContents.title}</h1>
         <h2 className={styles.subtitle}>{pageContents.subtitle}</h2>
         <List {...listData} />
+        <div className={styles.creator}>
+          <Creator text={settings.listCreatorText} action={title => this.addList(title)} />
+        </div>
       </main>
     )
   }
