19924b3e6cf395624f8a2a0c5b68bdc0d257582a
Grzegorz Jodłowski <grzegorz.jodlowski@gmail.com>
2020-02-12 16:51:11 +0100

    Finish cards search function

--- src/redux/searchStringRedux.js
+++ src/redux/searchStringRedux.js
@@ -5,16 +5,20 @@ export const countAllCards = ({ cards }) => cards.length;
 
 
 // action name creator
-
-
+const reducerName = 'searchString';
+const createActionName = name => `app/${reducerName}/${name}`;
 
 // actions types
+export const CHANGE = createActionName('CHANGE');
 
 // action creators
+export const createAction_changeSearchString = payload => ({ payload: payload, type: CHANGE });
 
 // reducer
 export default function reducer(statePart = '', action = {}) {
   switch (action.type) {
+    case CHANGE:
+      return action.payload;
     default:
       return statePart;
   }
